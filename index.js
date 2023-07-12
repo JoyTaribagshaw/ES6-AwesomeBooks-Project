f5d160e202ee0a3bdf020560ac006c9846f8836c
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-07-12 09:27:13 +0100

    Update js file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { links, containers, bookHeading } from './modules/variables.js';
 import dateData from './modules/DateAndTime.js';
+import BookStore from './modules/bookstore.js';
 
 const hideAllContainers = () => {
   containers.forEach((container) => {
@@ -42,64 +43,5 @@ window.addEventListener('DOMContentLoaded', () => {
   containers[0].style.display = 'block';
   links[0].classList.add('show--active--link');
 });
-
-class BookStore {
-  constructor() {
-    this.bookStore = JSON.parse(localStorage.getItem('bookItem')) || [];
-    this.submitButton = document.getElementById('submit');
-    this.bookListDiv = document.getElementById('bookList');
-
-    this.submitButton.addEventListener('click', this.addBook.bind(this));
-    this.displayBooks();
-  }
-
-  displayBooks = () => {
-    this.bookListDiv.innerHTML = '';
-
-    for (let i = 0; i < this.bookStore.length; i += 1) {
-      const book = this.bookStore[i];
-
-      const bookElement = document.createElement('div');
-      bookElement.classList.add('book');
-      bookElement.innerHTML = `
-        <p>${book.bookTitle}</p>
-        <p>${book.bookAuthor}</p>
-        <button class="remove-button">Remove</button>
-      `;
-
-      const removeButton = bookElement.querySelector('.remove-button');
-      removeButton.addEventListener('click', () => {
-        this.removeBook(i);
-      });
-
-      this.bookListDiv.appendChild(bookElement);
-      this.checkForBookstoreLength();
-    }
-  }
-
-  addBook = () => {
-    const bookTitle = document.getElementById('title').value;
-    const bookAuthor = document.getElementById('author').value;
-    const newBook = { bookTitle, bookAuthor };
-    this.bookStore.push(newBook);
-    localStorage.setItem('bookItem', JSON.stringify(this.bookStore));
-    this.displayBooks();
-    this.clearForm();
-  }
-
-  removeBook = (index) => {
-    this.bookStore.splice(index, 1);
-    localStorage.setItem('bookItem', JSON.stringify(this.bookStore));
-    this.displayBooks();
-    this.checkForBookstoreLength();
-  }
-
-  checkForBookstoreLength = () => (this.bookStore.length > 0 ? this.bookListDiv.classList.add('showBookListBorder') : this.bookListDiv.classList.remove('showBookListBorder'))
-
-  clearForm = () => {
-    document.getElementById('title').value = '';
-    document.getElementById('author').value = '';
-  }
-}
 
 const bookStore = new BookStore();
